046b71513ecd40662095dcfd0578007b93dfafd1
Egogorka <bike034@mail.ru>
2020-12-11 11:22:59 +0300

    now you can launch from main.py

--- heapling/main.py
+++ heapling/main.py
@@ -1,32 +1,4 @@
-import pygame
-from model.scene import Scene
-from utility.vector import Vector
+from controllers.start_controller import StartController
 
-pygame.init()
-screen = pygame.display.set_mode((1500, 1000))
-surface = pygame.Surface((1500, 1000))
-FPS = 50
-
-scene = Scene(Vector(0, 0), Vector(110,0), 1500, 1000)
-scene.start()
-
-clock = pygame.time.Clock()
-finished = False
-i = -1
-
-
-while not finished:
-    i += 1
-    clock.tick(FPS)
-    scene.step(1/FPS)
-    screen.blit(scene.display(), (scene.get_cords().x, scene.get_cords().y))
-    pygame.display.update()
-    if i==10:
-        scene.heap_jump()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                scene.heap_jump() 
+app = StartController(1400, 800)
+app.run()
--- heapling/test.py
+++
@@ -1,4 +0,0 @@
-from controllers.start_controller import StartController
-
-app = StartController(1400, 800)
-app.run()
